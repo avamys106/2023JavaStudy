5ad8de9bf2949faeb455ccfccd2991ebaabce970
avamys106 <avamys106@naver.com>
2024-01-10 18:26:52 +0900

    2024-01-10 inheritance, interface

---
+++ src/ex04controlstatement/QuPuramid04.java
@@ -0,0 +1,17 @@
+package ex04controlstatement;
+
+public class QuPuramid04 {
+
+	public static void main(String[] args) {
+		
+		for(int x=1 ; x<=9 ; x++) {
+			
+			int y = 5;
+			if(x==y) {
+				System.out.print("*");
+				x--;
+			} 
+//			System.out.println("");
+		}
+	}
+}
